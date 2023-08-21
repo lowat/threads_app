d75818a15dd28341a78a8c79fe54b300254ad520
Logan Watkins <lw2014wa@gmail.com>
2023-08-20 19:43:00 -0500

    first commit

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,53 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from 'next/navigation';
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+
+async function Page() {
+
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    const result = await fetchUsers({
+      userId: user.id,
+      searchString: '',
+      pageNumber: 1,
+      pageSize: 25, 
+    })
+
+    if(!userInfo?.onboarded) redirect('/onboarding');
+    
+  return (
+    <section>
+        <h1 className='head-text mb-10'>
+            <div className="mt-14 flex flex-col gap-9">
+              {result.users.length === 0 ? (
+                <p className="no-result">No users</p>
+              ) : (
+                <>
+                  {result.users.map((person) => (
+                    <UserCard 
+                      key = {person.id}
+                      id={person.id}
+                      name={person.name}
+                      username={person.username}
+                      imgUrl={person.image}
+                      personType = 'User'
+                    />
+                  ))}
+                </>
+              )}
+            </div>
+        </h1>
+    </section>
+  )
+};
+
+export default Page;
